ddd27a272aadab495308c1518b321db1b6bdf824
Sebastian <sebgertz@gmail.com>
2018-08-02 10:11:57 -0400

    console logs repos correctly

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const app = "I don't do much.";
+// const app = "I don't do much.";
+
+const token = '7fe2aceb9d8fb3e44f18ccd7bf73aa5c15fd97a8'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
